2575f08a663be610838595d14358d6804cfdde9b
Jae Shin <sjs870501@gmail.com>
2022-02-26 00:32:28 +1100

    commit

--- demo-3/vars.tf
+++ demo-3/vars.tf
@@ -5,15 +5,12 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-southeast-2"
 }
 
 variable "AMIS" {
-  type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    ap-southeast-2 = "ami-0a4e637babb7b0a86"
   }
 }
 
