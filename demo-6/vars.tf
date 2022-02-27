04f2aee89cfc1eeb3e3a30968103c76878da8b2a
Jae Shin <sjs870501@gmail.com>
2022-02-28 00:55:59 +1100

    commit

--- demo-6/vars.tf
+++ demo-6/vars.tf
@@ -1,5 +1,11 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
 variable "AWS_REGION" {
-  default = "eu-west-1"
+ default = "ap-southeast-2"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
